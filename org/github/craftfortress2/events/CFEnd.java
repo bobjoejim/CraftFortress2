c588032154212618094e52cb90423c4cd7286d0f
bobjoejim <alexz1178@yahoo.com>
2012-08-12 16:38:13 -0700

    Update org/github/craftfortress2/events/CFEnd.java

--- org/github/craftfortress2/events/CFEnd.java
+++ org/github/craftfortress2/events/CFEnd.java
@@ -1,6 +1,8 @@
 package org.github.craftfortress2;
-public class CFEnd{
-		public static void endGame(){
-			
-		}
+public class CFEnd {
+	public static void endGame(){
+		Player player = null;
+		Server server = player.getServer();
+		server.broadcastMessage("A game of Craft Fortress 2 has just ended!");	
+	}
 }
