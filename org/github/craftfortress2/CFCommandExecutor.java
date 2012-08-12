2d49bc5f0a8fe243c85facb266d4b546e06b40b2
ultimatepig <theultimatepig@gmail.com>
2012-08-12 13:35:08 -0700

    Update org/github/craftfortress2/CFCommandExecutor.java

--- org/github/craftfortress2/CFCommandExecutor.java
+++ org/github/craftfortress2/CFCommandExecutor.java
@@ -12,19 +12,18 @@ public class CFCommandExecutor implements CommandExecutor {
 			if (sender instanceof Player) {
 				player = (Player) sender;
 			}
-			if (cmd.getName().equalsIgnoreCase("cfstart")) {
+			if (cmd.getName().equalsIgnoreCase("cfstart")&& sender.hasPermission("cf.start")) {
 				CFStart.startGame();
 				return true;
 			}
-			if (cmd.getName().equalsIgnoreCase("cfend")) {
+			if (cmd.getName().equalsIgnoreCase("cfend")&& sender.hasPermission("cf.end")) {
 				CFEnd.endGame();
 				return true;
 			} //in future: add /cfjoin [TEAM], /cfspectate, etc.
-			//we also need some way to set arena boundaries, use worldedit?
-			if(cmd.getName().equalsIgnoreCase("cfhelp")){
+			if(cmd.getName().equalsIgnoreCase("cfhelp")&& sender.hasPermission("cf.help")){
 				sender.sendMessage("CRAFT FORTRESS 2 HELP");
-				sender.sendMessage("/cfstart - start a game of CraftFortress");
-				sender.sendMessage("/cfend - end a game of CraftFortress");
+				sender.sendMessage("Use /cfstart to start a game of CraftFortress");
+				sender.sendMessage("Use /cfend to end a game of CraftFortress");
 				return true;
 			}
 			if(args.length>1){
