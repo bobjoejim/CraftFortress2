599351d11a106aaa8fdf795a36bfe92f0188bbb4
bobjoejim <alexz1178@yahoo.com>
2012-08-12 14:53:59 -0700

    Added broadcasting

--- org/github/craftfortress2/CFEnd.java
+++ org/github/craftfortress2/CFEnd.java
@@ -1,8 +1,10 @@
 package org.github.craftfortress2;
-
+import org.bukkit.Server;
+import org.bukkit.entity.Player;
 public class CFEnd {
-	public static void endGame() {
-		//add more stuff later
-		//HOW ARE WE GONNA DO THIS D:
-	}
+		public static void endGame(){
+			Player player = null;
+			Server server = player.getServer();
+			server.broadcastMessage("A CraftFortress2 game has just ended!");
+		}
 }
