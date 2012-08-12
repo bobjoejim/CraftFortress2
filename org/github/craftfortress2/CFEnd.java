435057727b18a488b555c3e8cc59a58d3e745700
Bobjoejim <alexz1178@yahoo.com>
2012-08-12 13:19:52 -0700

    First Commit
    Add more stuff later

---
+++ org/github/craftfortress2/CFEnd.java
@@ -0,0 +1,8 @@
+package org.github.cf2;
+
+public class CFEnd {
+	public static void endGame() {
+		//add more stuff later
+		//HOW ARE WE GONNA DO THIS D:
+	}
+}
---
+++ org/github/craftfortress2/CFStart.java
@@ -0,0 +1,8 @@
+package org.github.cf2;
+
+public class CFStart {
+	public static void startGame() {
+		//add more stuff later
+		//HOW ARE WE GONNA DO THIS D:
+	}
+}
