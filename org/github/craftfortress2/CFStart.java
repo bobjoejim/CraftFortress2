bd74a346608364faa4e81b84e5c615a8f5c227dd
bobjoejim <alexz1178@yahoo.com>
2012-08-19 17:18:04 -0700

    Apparently == doesn't really work for strings. O.o

--- org/github/craftfortress2/CFStart.java
+++ org/github/craftfortress2/CFStart.java
@@ -17,38 +17,38 @@ public class CFStart extends CFCommandExecutor {
 			Location blue = new Location(dustbowl, 1, 90, 1);
 			Location red = new Location(dustbowl, 2, 90, 2);
 			for (int i=0;i<players.length;i++)	{
-				if (CFCommandExecutor.getTeam(players[i]) == "blue") {
+				if (CFCommandExecutor.getTeam(players[i]).equals("blue")) {
 					players[i].teleport(blue);
 				}else{
 					players[i].teleport(red);
 			}
-				if(CFCommandExecutor.getClass(players[i]) == "scout"){
-					
-				}
-				if(CFCommandExecutor.getClass(players[i]) == "soldier"){
-					
-				}
-				if(CFCommandExecutor.getClass(players[i]) == "demoman"){
-					
-				}
-				if(CFCommandExecutor.getClass(players[i]) == "heavy"){
-					
-				}
-				if(CFCommandExecutor.getClass(players[i]) == "medic"){
-					
-				}
-				if(CFCommandExecutor.getClass(players[i]) == "engineer"){
-					
-				}
-				if(CFCommandExecutor.getClass(players[i]) == "spy"){
-					
-				}
-				if(CFCommandExecutor.getClass(players[i]) == "pyro"){
-					
-				}
-				if(CFCommandExecutor.getClass(players[i]) == "sniper"){
-					
-				}
+			if(CFCommandExecutor.getClass(players[i]).equals("scout")){
+
+			}
+			if(CFCommandExecutor.getClass(players[i]).equals("soldier")){
+
+			}
+			if(CFCommandExecutor.getClass(players[i]).equals("demoman")){
+
+			}
+			if(CFCommandExecutor.getClass(players[i]).equals("heavy")){
+
+			}
+			if(CFCommandExecutor.getClass(players[i]).equals("medic")){
+
+			}
+			if(CFCommandExecutor.getClass(players[i]).equals("engineer")){
+
+			}
+			if(CFCommandExecutor.getClass(players[i]).equals("spy")){
+
+			}
+			if(CFCommandExecutor.getClass(players[i]).equals("pyro")){
+
+			}
+			if(CFCommandExecutor.getClass(players[i]).equals("sniper")){
+
+			}
 		}
 	}
 }
