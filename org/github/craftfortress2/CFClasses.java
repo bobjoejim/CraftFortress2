2e9fced9399aa9e751c00604d477e216c65f6fdc
ultimatepig <theultimatepig@gmail.com>
2012-08-20 18:05:44 -0700

    Update org/github/craftfortress2/CFClasses.java

--- org/github/craftfortress2/CFClasses.java
+++ org/github/craftfortress2/CFClasses.java
@@ -1,32 +1,14 @@
 package org.github.craftfortress2;
-
-import org.bukkit.command.CommandSender;
-import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
-import org.bukkit.event.entity.EntityRegainHealthEvent;
 import org.bukkit.event.entity.FoodLevelChangeEvent;
-
 public class CFClasses {
-
-	public void onFoodLevelChange(FoodLevelChangeEvent event) {
-		if (event.getEntity() instanceof Player) {
-			Player player = (Player) event.getEntity();
+	@EventHandler
+	public void onFoodBarLower(FoodLevelChangeEvent event){
+			if(event.getEntity()instanceof Player){
+				Player player = (Player)event.getEntity();
 				player.setExhaustion(0);
-				event.setFoodLevel(19);
-
-				event.setCancelled(true);
+				player.setFoodLevel(17);
+			}
 		}
 	}
-	 @EventHandler // THIS IS THE CODE OF NOREGEN, NOT OURS
-	  public void onHeal(EntityRegainHealthEvent event) {
-		 if (event.getEntityType().equals(EntityType.PLAYER)) {
-			 Player p = (Player) event.getEntity();
-			 if (event.getRegainReason().equals(EntityRegainHealthEvent.RegainReason.SATIATED)) {
-				 event.setCancelled(true);
-			 }
-	 		} else if (event.getRegainReason().equals(EntityRegainHealthEvent.RegainReason.SATIATED)) {
-	 			event.setCancelled(true);
-	        }
-	 }
-}
