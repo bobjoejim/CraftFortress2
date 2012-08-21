c28236ed2a01d1d8eced26a3349d21351a6e2d26
ultimatepig <theultimatepig@gmail.com>
2012-08-20 18:09:44 -0700

    Update org/github/craftfortress2/Scout.java

--- org/github/craftfortress2/Scout.java
+++ org/github/craftfortress2/Scout.java
@@ -4,14 +4,13 @@ import org.bukkit.entity.Player;
 import org.bukkit.potion.*;
 import org.bukkit.inventory.*;
 public class Scout extends CFClasses {
-	Player player = null;
-	PlayerInventory inv = player.getInventory();
-	public void init(){
+	public static void init(){
+		Player player = null;
+		PlayerInventory inv = player.getInventory();
 		player.setFoodLevel(17);
 		player.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 999999999, 1));
 		inv.setHelmet(new ItemStack(Material.IRON_HELMET, 1));
 		inv.setBoots(new ItemStack(Material.IRON_BOOTS, 1));
 		inv.setItem(38, new ItemStack(Material.STICK, 1));
 	}
-
 }
