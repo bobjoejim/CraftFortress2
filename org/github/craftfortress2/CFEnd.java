89ba845ac0e88ef0a2801b23be4efdfa9d8ab01a
Bobjoejim <alexz1178@yahoo.com>
2012-08-22 22:44:57 -0700

    Fixed ALL THE BUGS! :D
    Except for /cfjoin and /cfstart. Also fixed formatting in plugin.yml and
    internal server errors in CFEnd and CFStart.

--- org/github/craftfortress2/CFEnd.java
+++ org/github/craftfortress2/CFEnd.java
@@ -1,10 +1,12 @@
 package org.github.craftfortress2;
+import org.bukkit.Bukkit;
 import org.bukkit.Server;
 import org.bukkit.entity.Player;
 public class CFEnd {
 	public static void endGame(){
 		Player player = null;
-		Server server = player.getServer();
+		//Server server = player.getServer();
+		Server server = Bukkit.getServer();
 		server.broadcastMessage("A game of Craft Fortress 2 has just ended!");	
 	}
 }
--- org/github/craftfortress2/CFStart.java
+++ org/github/craftfortress2/CFStart.java
@@ -1,54 +1,57 @@
 package org.github.craftfortress2;
+import org.bukkit.Bukkit;
 import org.bukkit.GameMode;
 import org.bukkit.Location;
 import org.bukkit.Server;
 import org.bukkit.World;
 import org.bukkit.entity.Player;
+import java.util.ArrayList;
 public class CFStart extends CFCommandExecutor {
 	public CFStart(CraftFortress2 cf2) {
 		super(cf2);
 	}
-	static Player[] players = (Player[]) CFCommandExecutor.players.toArray();
 	public static void startGame(){
+		//Player[] players = (Player[]) CFCommandExecutor.players.toArray();
+		ArrayList<Player> players = CFCommandExecutor.players;
 		Player player = null;
-		Server server = player.getServer();
+		//Server server = player.getServer();
+		Server server = Bukkit.getServer();
 		server.broadcastMessage("Craft Fortress 2 is starting!");
-		Server svr = player.getServer();
-		World dustbowl = svr.getWorld("AT_Dustbowl");
+		World dustbowl = server.getWorld("AT_Dustbowl");
 		Location blue = new Location(dustbowl, 1, 90, 1);
 		Location red = new Location(dustbowl, 2, 90, 2);
-		for (int i=0;i<players.length;i++)	{
-			players[i].setGameMode(GameMode.ADVENTURE);
-			if (CFCommandExecutor.getTeam(players[i]).equals("blue")) {
-				players[i].teleport(blue);
+		for (int i=0;i<players.size();i++)	{
+			players.get(i).setGameMode(GameMode.ADVENTURE);
+			if (CFCommandExecutor.getTeam(players.get(i)).equals("blue")) {
+				players.get(i).teleport(blue);
 			}else{
-				players[i].teleport(red);
+				players.get(i).teleport(red);
 			}
-			if(CFCommandExecutor.getClass(players[i]).equals("scout")){
+			if(CFCommandExecutor.getClass(players.get(i)).equals("scout")){
 				Scout.init();
 			}
-			if(CFCommandExecutor.getClass(players[i]).equals("soldier")){
+			if(CFCommandExecutor.getClass(players.get(i)).equals("soldier")){
 
 			}
-			if(CFCommandExecutor.getClass(players[i]).equals("demoman")){
+			if(CFCommandExecutor.getClass(players.get(i)).equals("demoman")){
 
 			}
-			if(CFCommandExecutor.getClass(players[i]).equals("heavy")){
+			if(CFCommandExecutor.getClass(players.get(i)).equals("heavy")){
 
 			}
-			if(CFCommandExecutor.getClass(players[i]).equals("medic")){
+			if(CFCommandExecutor.getClass(players.get(i)).equals("medic")){
 
 			}
-			if(CFCommandExecutor.getClass(players[i]).equals("engineer")){
+			if(CFCommandExecutor.getClass(players.get(i)).equals("engineer")){
 
 			}
-			if(CFCommandExecutor.getClass(players[i]).equals("spy")){
+			if(CFCommandExecutor.getClass(players.get(i)).equals("spy")){
 
 			}
-			if(CFCommandExecutor.getClass(players[i]).equals("pyro")){
+			if(CFCommandExecutor.getClass(players.get(i)).equals("pyro")){
 
 			}
-			if(CFCommandExecutor.getClass(players[i]).equals("sniper")){
+			if(CFCommandExecutor.getClass(players.get(i)).equals("sniper")){
 
 			}
 		}
