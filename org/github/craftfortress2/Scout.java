8497efac9d1c828631ff388857a21e1eb557819b
bobjoejim <alexz1178@yahoo.com>
2012-08-26 15:09:06 -0700

    Added inventory saving(hopefully)

--- org/github/craftfortress2/Scout.java
+++ org/github/craftfortress2/Scout.java
@@ -6,11 +6,12 @@ import org.bukkit.inventory.*;
 public class Scout extends CFClasses {
 	public static void init(Player player){
 		PlayerInventory inv = player.getInventory();
+		saveInv(player);
 		inv.clear();
 		player.setFoodLevel(17);
 		player.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 99999999, 1));
 		inv.setHelmet(new ItemStack(Material.IRON_HELMET, 1));
 		inv.setBoots(new ItemStack(Material.IRON_BOOTS, 1));
-		inv.setItem(inv.firstEmpty(), new ItemStack(Material.STICK, 1));
+		inv.setItem(inv.getHeldItemSlot(), new ItemStack(Material.STICK, 1));
 	}
 }
