d240a2698c2098dbe17c4cbc104041361aa082e3
ultimatepig <theultimatepig@gmail.com>
2012-08-26 13:07:29 -0700

    Update org/github/craftfortress2/CFStart.java

--- org/github/craftfortress2/CFStart.java
+++ org/github/craftfortress2/CFStart.java
@@ -6,6 +6,7 @@ import org.bukkit.Server;
 import org.bukkit.World;
 import org.bukkit.entity.Player;
 import java.util.ArrayList;
+import java.util.Random;
 public class CFStart extends CFCommandExecutor {
 	public CFStart(CraftFortress2 cf2) {
 		super(cf2);
@@ -13,19 +14,30 @@ public class CFStart extends CFCommandExecutor {
 	public static void startGame(){
 		//Player[] players = (Player[]) CFCommandExecutor.players.toArray();
 		ArrayList<Player> players = CFCommandExecutor.players;
-		Player player = null;
 		//Server server = player.getServer();
 		Server server = Bukkit.getServer();
 		server.broadcastMessage("Craft Fortress 2 is starting!");
-		World dustbowl = server.getWorld("AT_Dustbowl");
-		Location blue = new Location(dustbowl, 1, 90, 1);
-		Location red = new Location(dustbowl, 2, 90, 2);
+		World ctf2fort = server.getWorld("ctf_2fort");
+		Location blue1 = new Location(ctf2fort, 78.52184, 70, 209.57359);
+		Location blue2 = new Location(ctf2fort, 31.80711, 70, 201.99570);
+		Location red1 = new Location(ctf2fort, 37.71015, 70, 256.51407);
+		Location red2 = new Location(ctf2fort, 84.70609, 70, 264.01575);
+		Random rand = new Random();
+		int spawn = rand.nextInt(2);
 		for (int i=0;i<players.size();i++)	{
 			players.get(i).setGameMode(GameMode.ADVENTURE);
 			if (CFCommandExecutor.getTeam(players.get(i)).equals("blue")) {
-				players.get(i).teleport(blue);
-			}else{
-				players.get(i).teleport(red);
+				if(spawn == 0){
+					players.get(i).teleport(blue1);
+				}else{
+					players.get(i).teleport(blue2);
+				}
+			}else if(CFCommandExecutor.getTeam(players.get(i)).equals("red")){
+				if(spawn == 0){
+					players.get(i).teleport(red1);
+				}else{
+					players.get(i).teleport(red2);
+				}
 			}
 			if(CFCommandExecutor.getClass(players.get(i)).equals("scout")){
 				Scout.init();
