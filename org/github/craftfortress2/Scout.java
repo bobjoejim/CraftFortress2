06026a6a36dc0d673536cfb8f45b219f6dfe848f
bobjoejim <alexz1178@yahoo.com>
2012-08-26 14:12:50 -0700

    FIXED HOPEFULLY

--- org/github/craftfortress2/Scout.java
+++ org/github/craftfortress2/Scout.java
@@ -6,10 +6,11 @@ import org.bukkit.inventory.*;
 public class Scout extends CFClasses {
 	public static void init(Player player){
 		PlayerInventory inv = player.getInventory();
+		inv.clear();
 		player.setFoodLevel(17);
-		player.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 999999999, 1));
+		player.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 99999999, 1));
 		inv.setHelmet(new ItemStack(Material.IRON_HELMET, 1));
 		inv.setBoots(new ItemStack(Material.IRON_BOOTS, 1));
-		inv.setItem(38, new ItemStack(Material.STICK, 1));
+		inv.setItem(inv.firstEmpty(), new ItemStack(Material.STICK, 1));
 	}
 }
