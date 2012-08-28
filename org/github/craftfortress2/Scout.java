8457d49f63c66d789334d0d4c1e52c459eaff093
bobjoejim <alexz1178@yahoo.com>
2012-08-27 22:31:06 -0700

    Fixed inventories

--- org/github/craftfortress2/Scout.java
+++ org/github/craftfortress2/Scout.java
@@ -3,15 +3,17 @@ import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.potion.*;
 import org.bukkit.inventory.*;
-public class Scout extends CFClasses {
+public class Scout {
 	public static void init(Player player){
 		PlayerInventory inv = player.getInventory();
-		saveInv(player);
 		inv.clear();
 		player.setFoodLevel(17);
 		player.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 99999999, 1));
 		inv.setHelmet(new ItemStack(Material.IRON_HELMET, 1));
 		inv.setBoots(new ItemStack(Material.IRON_BOOTS, 1));
-		inv.setItem(inv.getHeldItemSlot(), new ItemStack(Material.STICK, 1));
+		inv.setItem(2, new ItemStack(Material.STICK, 1));
+	}
+	public static void a(Player p){
+		CFStart.a(p);
 	}
 }
