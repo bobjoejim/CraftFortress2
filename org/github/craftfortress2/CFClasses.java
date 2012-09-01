31f0118d3c0774062c852a80c03728943ee22185
bobjoejim <alexz1178@yahoo.com>
2012-08-31 20:36:04 -0700

    oops

--- org/github/craftfortress2/CFClasses.java
+++ org/github/craftfortress2/CFClasses.java
@@ -1,46 +1,82 @@
 package org.github.craftfortress2;
 import org.bukkit.Bukkit;
-import org.bukkit.GameMode;
 import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.Server;
-import org.bukkit.World;
-import java.util.Random;
-public class CFStart extends CFCommandExecutor {
-	static boolean gameHasStarted = false;
-	public CFStart(CraftFortress2 cf2) {
-		super(cf2);
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
+import org.bukkit.event.*;
+import org.bukkit.event.entity.*;
+import org.bukkit.event.player.*;
+import org.bukkit.inventory.ItemStack;
+public class CFClasses implements Listener{
+	@EventHandler
+	public void onHungerBarChange(FoodLevelChangeEvent event){
+		event.setCancelled(true);
 	}
-	public static void startGame(){
-		Server server = Bukkit.getServer();
-		server.broadcastMessage("Craft Fortress 2 is starting!");
-		World ctf2fort = server.getWorld("ctf_2fort");
-		Location blu1 = new Location(ctf2fort, 78, 70, 209);
-		Location blu2 = new Location(ctf2fort, 31, 70, 201);
-		Location red1 = new Location(ctf2fort, 37, 70, 256);
-		Location red2 = new Location(ctf2fort, 84, 70, 264);
-		Random rand = new Random();
-		int spawn = rand.nextInt(2);
-		for (int i=0;i<players.size();i++){
-			players.get(i).setGameMode(GameMode.ADVENTURE);
-			players.get(i).setHealth(20);
-			if (players.get(i).toString().equals(changeTeam(CFCommandExecutor.team))){
-				players.get(i).teleport(new Location(ctf2fort, 0, 0, 0));
-			}
-			if (getTeam(players.get(i)).equals("blu")) {
-				if(spawn == 0){
-					players.get(i).teleport(blu1);
-				}else{
-					players.get(i).teleport(blu2);
-				}
-			}else if(getTeam(players.get(i)).equals("red")){
-				if(spawn == 0){
-					players.get(i).teleport(red1);
-				}else{
-					players.get(i).teleport(red2);
-				}
-			}
-			initClasses(players.get(i));
+	@EventHandler
+	public void onPickupItem(PlayerPickupItemEvent event){
+		Player player = event.getPlayer();
+		if(CFCommandExecutor.isPlaying(player) && event.getItem().equals(Material.BOOK)){
+			event.setCancelled(false);
+		}else if(CFCommandExecutor.isPlaying(player)){
+			event.setCancelled(true);
 		}
-		gameHasStarted = true;
+	}
+	@EventHandler
+	public void pvp(EntityDamageByEntityEvent event){
+		Entity damager = event.getDamager();
+		Entity player = event.getEntity();
+			if(CFCommandExecutor.getTeam((Player) damager) == CFCommandExecutor.getTeam((Player) player)){
+				event.setCancelled(true);
+			}
+		}
+	@EventHandler
+	public void dropItem(PlayerDropItemEvent event){
+		Player player = event.getPlayer();
+		if(CFCommandExecutor.isPlaying(player)){
+			event.setCancelled(true);
+		}
+	}
+	@EventHandler
+	public void onPlayerRespawn(PlayerRespawnEvent event){
+		Player player = event.getPlayer();
+		Server server = Bukkit.getServer();
+		if(player.equals(CFCommandExecutor.changeTeam(CFCommandExecutor.team))){
+			player.teleport(new Location(server.getWorld("ctf_2fort"), 0, 0, 0));
+		}
+		if(CFCommandExecutor.isPlaying(player)){
+			if(CFCommandExecutor.getClass(player).equals("scout")){
+				Scout.init(player);
+				return;
+			}else if(CFCommandExecutor.getClass(player).equals("soldier")){
+
+			}else if(CFCommandExecutor.getClass(player).equals("heavy")){
+
+			}else if(CFCommandExecutor.getClass(player).equals("demoman")){
+
+			}else if(CFCommandExecutor.getClass(player).equals("medic")){
+
+			}else if(CFCommandExecutor.getClass(player).equals("pyro")){
+
+			}else if(CFCommandExecutor.getClass(player).equals("sniper")){
+
+			}else if(CFCommandExecutor.getClass(player).equals("engineer")){
+
+			}else if(CFCommandExecutor.getClass(player).equals("spy")){
+
+			}
+		}
+	}
+	public static void loadInv(Player player){
+		player.getInventory().clear();
+		player.getInventory().setContents(getSavedInv(player));
+	}
+	public static ItemStack[] getSavedInv(Player player){
+		int index = CFCommandExecutor.players.lastIndexOf(player);
+		if (index == -1){
+			return null;
+		}
+		return CFCommandExecutor.saveItemStack.get(index);
 	}
 }
